e86b68fe0af3d112aec23b56c68c0f630a1781fb
huaminglin <40079294+huaminglin@users.noreply.github.com>
2018-12-26 06:24:21 -0500

    Use docker compose to improve mysql-demo

--- mysql-demo/update.sh
+++ mysql-demo/update.sh
@@ -2,22 +2,11 @@
 
 cd $(dirname $0)
 
-docker stop demomysql
-docker rm demomysql
-docker create --name demomysql -e MYSQL_ROOT_PASSWORD=demo -p 6603:3306 mysql/mysql-server:5.7  --character-set-server=utf8 --collation-server=utf8_general_ci
+docker-compose down
+docker-compose up --no-start
+docker-compose start
 
+docker-compose exec server bash -c "touch /var/log/mysql.general.log; chown mysql:mysql /var/log/mysql.general.log"
+# It seems that the above command is executed before mysql daemon starts and the general log is ON without restart server.
 
-if [ -z $DOCKER_MYSQL_CONFIG ]; then
-    DOCKER_MYSQL_CONFIG="$*"
-fi
-
-if [ -z $DOCKER_MYSQL_CONFIG ]; then
-    DOCKER_MYSQL_CONFIG=my.cnf.origin
-fi
-
-echo DOCKER_MYSQL_CONFIG: $DOCKER_MYSQL_CONFIG
-docker cp $DOCKER_MYSQL_CONFIG demomysql:/etc/my.cnf
-docker start demomysql
-docker exec demomysql touch /var/log/mysql.general.log
-docker exec demomysql chown mysql.mysql /var/log/mysql.general.log
-docker restart demomysql
+# docker-compose exec server mysql -pdemo mysql -e "show variables like \"%general%\""
