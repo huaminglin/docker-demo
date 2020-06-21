ed91e4a36b7d550f65c243e9971efb419157ae68
huaminglin <40079294+huaminglin@users.noreply.github.com>
2020-06-21 15:06:54 +0800

    Update postgres-transaction-demo/sql/serialization-anomaly to include pg_locks info

--- postgres-demo/postgres-transaction-demo/sql/serialization-anomaly-1.sql
+++ postgres-demo/postgres-transaction-demo/sql/serialization-anomaly-1.sql
@@ -1,6 +1,8 @@
 BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+select 'sql 1', txid_current();
 insert into mytab(class, value) select 2, SUM(value) FROM mytab WHERE class = 1;
+select 'sql 1', * from pg_locks;
 
-select pg_sleep(2);
+select pg_sleep(1);
 select 'sql 1: commit';
 commit;
--- postgres-demo/postgres-transaction-demo/sql/serialization-anomaly-2.sql
+++ postgres-demo/postgres-transaction-demo/sql/serialization-anomaly-2.sql
@@ -1,6 +1,8 @@
 select pg_sleep(0.1); -- make sure this transaction starts after the first transaction.
 BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+select 'sql 2', txid_current();
 insert into mytab(class, value) select 1, SUM(value) FROM mytab WHERE class = 2;
+select 'sql 2', * from pg_locks;
 
 select pg_sleep(2);
 select 'sql 2: commit';
