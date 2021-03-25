23e5c5e52ea7c4ce7d62d74e82a17e99c2455fcc
huaminglin <40079294+huaminglin@users.noreply.github.com>
2021-03-25 09:17:50 -0400

    Add PerfCounter.print

--- jdk-demo/perfdata-demo/update.sh
+++ jdk-demo/perfdata-demo/update.sh
@@ -7,5 +7,5 @@ perfdata="-XX:+UsePerfData"
 
 docker stop demoperf
 docker rm demoperf
-docker create --name demoperf -e JAVA_OPTS="$perfdata" -v /tmp/demooql:/tmp/demoperf -p 18030:8080 tomcat:9.0.43
+docker create --name demoperf -e JAVA_OPTS="$perfdata" -v /tmp/demoperf:/tmp/demoperf -p 18030:8080 tomcat:9.0.43
 docker start demoperf
